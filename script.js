b5204a39ed822febe310ebeb9f040e49a0b03455
mwakamHassan <mwakamshahassan0@gmail.com>
2025-04-11 00:50:08 +0300

    Added Files

---
+++ script.js
@@ -0,0 +1,11 @@
+// script.js
+
+// Example: Display a greeting message when the page loads
+window.onload = function() {
+  alert("Welcome to my portfolio website!");
+};
+
+// Example: Add a click event to the "Learn More About Me" button
+document.querySelector('.cta-button').addEventListener('click', function() {
+  alert("You clicked the 'Learn More' button!");
+});
